8ced117e1c12b35d358d60a35f332c9dab8e8369
KirilPetruchak <kiril.petruchak@ulito.co.uk>
2017-09-23 17:45:18 +0300

    add Repositories project

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF;
+
+namespace Repositories
+{
+	public class BaseRepository
+	{
+		protected HostingEntities _db { get; set; }
+
+		public BaseRepository()
+		{
+			_db = new HostingEntities();
+		}
+	}
+}
