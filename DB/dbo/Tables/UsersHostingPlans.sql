3e27d5e12d05c443cee3f38c8b51f6e612bd16e1
Anton Pukhno <pukhno.anton@gmail.com>
2017-09-23 17:19:46 +0300

    Renamed Users_HostingPlans to UsersHostingPlans

---
+++ DB/dbo/Tables/UsersHostingPlans.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UsersHostingPlans]
+(
+	[ID] INT NOT NULL IDENTITY(1,1),
+	[UserID] INT NOT NULL,
+	[HostingPlanID] INT NOT NULL,
+	[CreatedDate] DATETIME NOT NULL,
+	[NextBillingDate] DATETIME NOT NULL,
+	[Active] BIT DEFAULT(1) NOT NULL,
+	CONSTRAINT [PK_UsersHostingPlans_ID] PRIMARY KEY ([ID] ASC),
+	CONSTRAINT [FK_UsersHostingPlans_Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [Users]([ID]),
+	CONSTRAINT [FK_UsersHostingPlans_HostingPlans_HostingPlanID] FOREIGN KEY ([HostingPlanID]) REFERENCES [HostingPlans]([ID]),
+)
