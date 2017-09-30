174bc38145c6b3d983347b16175a96d8c5248de2
Anton Pukhno <pukhno.anton@gmail.com>
2017-09-30 14:48:56 +0300

    Getting Hosting Plans from DB

---
+++ Repositories/HostingPlansRepository.cs
@@ -0,0 +1,19 @@
+﻿using EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class HostingPlansRepository : BaseRepository
+    {
+        public async Task<List<HostingPlan>> GetHostingPlansAsync()
+        {
+            return await _db.HostingPlans.Include(x => x.Features).Include(x => x.HostingPlansPrices).Where(x => x.Active == true).ToListAsync();
+            //return await _db.HostingPlans.Where(x => x.Active == true).ToListAsync();
+        }
+    }
+}
