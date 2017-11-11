a89337fe83a1d315458470667332940bee8b5083
KirilPetruchak <kiril.petruchak@ulito.co.uk>
2017-11-11 13:31:44 +0200

    hash submit

--- Hosting/Controllers/HomeController.cs
+++ Hosting/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Hosting.Controllers
 {
@@ -27,10 +29,16 @@ namespace Hosting.Controllers
             });
         }
 
+		public string Hash(string value)
+		{
+			UTF8Encoding encoding = new UTF8Encoding();
+			return Convert.ToBase64String(new HMACMD5(encoding.GetBytes("flk3409refn54t54t*FNJRET")).ComputeHash(encoding.GetBytes("test_merch_n1www.market.uaSimpleSignatureDH78302314153798631547.36UAHПроцессор Intel Core i5-4670 3.4GHz10001ВасяВасечкинsome@mail.com380631234567")));
+		}
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-            return View();
+			ViewBag.Message = "Your application description page.";
+			return View();
         }
 
         public ActionResult Contact()
